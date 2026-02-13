6aa153e4888f47e9c54045ce3462840fe9dde849
thaparojanthapa1-eng <thaparojanthapa1@gmail.com>
2026-02-13 19:30:34 +0545

    Add Account class with deposit, withdrawal, and balance check methods

--- account.py
+++ account.py
@@ -1,6 +1,16 @@
-class account:
-    def __init__(self):
-        pass
+class Account:
+    def __init__(self, customer_id, balance):
+        self.customer_id=customer_id
+        self.balance=balance
 
-    def new_account_details(self):
-        pass
+    def balance_check(self):
+        return self.balance
+
+    def deposit(self, amount):
+        self.balance+=amount
+
+    def withdrawal(self, amount):
+        if self.balance<amount:
+            print("Insufficient balance")
+        else:
+            self.balance-=amount
