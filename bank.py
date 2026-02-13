ff4dd378ae36607cab45b2af00efcf0cd682c71f
thaparojanthapa1-eng <thaparojanthapa1@gmail.com>
2026-02-13 18:36:44 +0545

    Changed the properties with methods as a stylistic choice

--- bank.py
+++ bank.py
@@ -2,23 +2,25 @@ class Bank:
     def __init__(self):
         self._customer_details = {}
 
-    @property
-    def customer_details(self):
+    def get_customer_details(self):
+        """Return all customer details."""
         return self._customer_details
 
-    @customer_details.setter
-    def customer_details(self, new_account_details):
+    def add_customer_details(self, new_account_details):
+        """Add or update customer details."""
         self._customer_details.update(new_account_details)
 
-    @customer_details.deleter
-    def customer_details(self):
+    def clear_customer_details(self):
+        """Delete all customer details."""
         print("Deleting all customer details...")
         self._customer_details.clear()
 
     def find_account(self, customer_id):
+        """Find a specific account by customer ID."""
         return self._customer_details.get(customer_id, "Account not found")
 
     def delete_account(self, customer_id):
+        """Delete a specific account by customer ID."""
         if customer_id in self._customer_details:
             del self._customer_details[customer_id]
             print(f"Deleted account {customer_id}")
