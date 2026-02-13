83b5523dad0b6f04e1719e0a8661a6b1eec0fa16
thaparojanthapa1-eng <thaparojanthapa1@gmail.com>
2026-02-13 18:44:43 +0545

    add Customer class with account linking support

--- customer.py
+++ customer.py
@@ -1,2 +1,23 @@
-class customer:
-    pass
+class Customer:
+    def __init__(self, customer_id, customer_name, customer_login, password,
+                 phone_no, email, address, citizenship_id, date_of_birth,
+                 marital_status=None, occupation=None, income_level=None,
+                 branch_id=None, nominee_name=None):
+        self.customer_id = customer_id
+        self.customer_name = customer_name
+        self.customer_login = customer_login
+        self.password = password
+        self.phone_no = phone_no
+        self.email = email
+        self.address = address
+        self.citizenship_id = citizenship_id
+        self.date_of_birth = date_of_birth
+        self.marital_status = marital_status
+        self.occupation = occupation
+        self.income_level = income_level
+        self.branch_id = branch_id
+        self.nominee_name = nominee_name
+        self.linked_accounts = []
+
+    def add_account(self, account):
+        self.linked_accounts.append(account)
