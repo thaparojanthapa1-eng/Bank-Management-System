c6e3773901b276c0f2f085e5444a78ecb6b39a35
thaparojanthapa1-eng <thaparojanthapa1@gmail.com>
2026-02-13 19:04:54 +0545

    add Transactions class with validation for transaction types

--- transactions.py
+++ transactions.py
@@ -1,2 +1,11 @@
-class transactions:
-    pass
+from datetime import datetime
+
+class Transactions:
+    def __init__(self, customer_id, transaction_amount, transaction_type):
+        self.customer_id=customer_id
+        self.transaction_amount=transaction_amount
+        allowed_transaction_types=["Withdrawal", "Deposit"]
+        if transaction_type in allowed_transaction_types:
+            self.transaction_type=transaction_type
+        else:
+            print(f"Select a valid transaction type:    {allowed_transaction_types}")
