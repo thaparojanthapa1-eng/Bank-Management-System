4f94fa3fbd7eefbfbbd43840cb79b358142a83c4
thaparojanthapa1-eng <thaparojanthapa1@gmail.com>
2026-02-14 19:04:20 +0545

    Fix Transactions class to ensure valid transaction_type

--- transactions.py
+++ transactions.py
@@ -4,8 +4,8 @@ class Transactions:
     def __init__(self, customer_id, transaction_amount, transaction_type):
         self.customer_id=customer_id
         self.transaction_amount=transaction_amount
-        allowed_transaction_types=["Withdrawal", "Deposit"]
+        allowed_transaction_types=["withdrawal", "deposit"]
         if transaction_type in allowed_transaction_types:
             self.transaction_type=transaction_type
         else:
-            print(f"Select a valid transaction type:    {allowed_transaction_types}")
+            self.transaction_type=input(f"Select a valid transaction type:    {allowed_transaction_types}").lower().strip()
