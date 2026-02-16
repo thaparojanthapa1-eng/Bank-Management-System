8fd04539f3ae58da61d1bc86ed045f0c2da0d7f3
thaparojanthapa1-eng <thaparojanthapa1@gmail.com>
2026-02-16 20:20:57 +0545

    Simplified Customer class

--- customer.py
+++ customer.py
@@ -7,17 +7,3 @@ class Customer:
         self.customer_name = customer_name
         self.customer_login = customer_login
         self.password = password
-        self.phone_no = phone_no
-        self.email = email
-        self.address = address
-        self.citizenship_id = citizenship_id
-        self.date_of_birth = date_of_birth
-        self.marital_status = marital_status
-        self.occupation = occupation
-        self.income_level = income_level
-        self.branch_id = branch_id
-        self.nominee_name = nominee_name
-        self.linked_accounts = []
-
-    def add_account(self, account):
-        self.linked_accounts.append(account)
