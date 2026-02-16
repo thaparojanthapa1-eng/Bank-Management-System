d9c06934944b858706d834627b5c1ea9e500ea98
thaparojanthapa1-eng <thaparojanthapa1@gmail.com>
2026-02-16 20:32:21 +0545

    Fixed minor issues

--- transactions.py
+++ transactions.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
 class Transactions:
-    def __init__(self, customer_id, transaction_amount, transaction_type):
+    def __init__(self, customer_id, transaction_amount):
         self.customer_id=customer_id
         self.transaction_amount=transaction_amount
