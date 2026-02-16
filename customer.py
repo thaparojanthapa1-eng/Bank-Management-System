20f691a27797c383c48e969a1d5ba8a27dc0dae9
thaparojanthapa1-eng <thaparojanthapa1@gmail.com>
2026-02-16 20:23:33 +0545

    Fixed minor issues

--- customer.py
+++ customer.py
@@ -1,8 +1,5 @@
 class Customer:
-    def __init__(self, customer_id, customer_name, customer_login, password,
-                 phone_no, email, address, citizenship_id, date_of_birth,
-                 marital_status=None, occupation=None, income_level=None,
-                 branch_id=None, nominee_name=None):
+    def __init__(self, customer_id, customer_name, customer_login, password,):
         self.customer_id = customer_id
         self.customer_name = customer_name
         self.customer_login = customer_login
